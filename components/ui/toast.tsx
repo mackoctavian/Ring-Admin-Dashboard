94cc0413363f33feb18b279d7bfa6efacbdd099b
Peter Karanja <pittskay@gmail.com>
2024-05-21 18:13:14 +0300

    Implemented success toast theme

--- components/ui/toast.tsx
+++ components/ui/toast.tsx
@@ -32,7 +32,7 @@ const toastVariants = cva(
         default: "border bg-background text-foreground",
         destructive: "destructive group border-destructive bg-destructive text-destructive-foreground",
         warning: "warning group border-yellow-200 bg-yellow-300 text-green-main",
-        success: "destructive group border-destructive bg-destructive text-destructive-foreground",
+        success: "success group border-green-600 bg-green-500 text-white",
         primary: "primary group border- bg-green text-green-foreground",
       },
     },
