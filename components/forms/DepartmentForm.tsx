00d4a45f6a1885bfacda079fc91cc91f858769b3
Walter Peter Karanja <pittskay@gmail.com>
2024-07-10 16:33:12 +0300

    Department form fix warnings

--- components/forms/DepartmentForm.tsx
+++ components/forms/DepartmentForm.tsx
@@ -60,7 +60,7 @@ const DepartmentForm = ({ item }: { item?: Department | null }) => {
     
         try {
             if (item) {
-                await updateItem(item!.$id, data);
+                await updateItem(item!.$id!, data);
                 toast({
                     variant: "success",
                     title: "Success", 
